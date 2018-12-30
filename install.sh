034fce89e8a7923eace5135cab5cfd034df4e787
Art Miller - KC7SDA <git@nouse4anick.com>
2018-12-30 14:48:35 -0800

    fixed code and major update
    forgot .tar.gz for the untaring, also there are issues with installing
    xastir from source.
    
    all code has been refactored into functions and command line has been
    implemented (-h for usage)

--- install.sh
+++ install.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
-#version: 1.1.2
+#version: 1.1.3
 # updated fresh install script, now installs everything needed for a portable digital station
 # Note: this is for the raspberry pi with a DRAWS Hat from nwdigitalradio
-#taken from the NW Digital Radio group wiki on installing fldigi
+
+# New update!
+# - functionised everything
+# - will have command line arguments (basic reinstall type stuff)
+# - 
+
+
 
 # Testing Variable: 1 for initial trial and tests/config file updates (does NOT copy config files)
 TESTING=1
+QUICK=0
 # set flags first taken from http://www.kk5jy.net/fldigi-build/:
 export CXXFLAGS='-O2 -march=native -mtune=native'
 export CFLAGS='-O2 -march=native -mtune=native'
 
-############################
-## BUILD INSTALL FUNCTION ##
-############################
-# pauses between configure/make/install to allow user to double check install progress
-Build_Install (){
-	#note: static linking enabled, possibly do not need it as other libraries get loaded.
-	#./configure --enable-static
-	./configure
-	read -n 1 -s -r -p "Press any key to continue"
-	echo
-	make
-	read -n 1 -s -r -p "Press any key to continue"
-	echo
-	sudo make install
-	read -n 1 -s -r -p "Press any key to continue"
-	echo
-}
 
 ######################
 ## Current versions ##
@@ -42,150 +32,275 @@ echo "flmsg: " $FLMSGCUR
 read -n 1 -s -r -p "Press any key to continue, ctrl+c to quit"
 echo
 
+##################
+## update/grade ##
+##################
+# always update and upgrade:
+sudo apt-get update
+sudo apt-get upgrade -y
+# favorate apps:
+sudo apt-get install geany -y
+# Needed items from the nwdigital scripts:
+sudo apt-get install rsync build-essential autoconf dh-autoreconf automake libtool git libasound2-dev libncurses5-dev -y
+
+############
+## params ##
+############
+
+# params are:
+# - fldigi
+# - flmsg
+# - flamp
+# - gps
+# - xastir (both xastir and direwolf
+# - chrony
+# 
+# if no params install everything
+
+if [ $# -gt 0 ]; then
+	#loop through commands and get them
+	while [ "$1" != "" ]; do
+		case $1 in
+			"FLDIGI" ) FLDIGI_source ;;
+			"FLAMP" ) FLAMP_source ;;
+			"FLMSG" ) FLMSG_source ;;
+			"GPS" ) GPSD_install ;;
+			"CHRONY" ) chrony_setup ;;
+			"xastir" ) Xastir_install ;;
+			-h ) usage ;;
+		esac
+		shift
+	done
+else
+	# install everything:
+	Enable_Sources
+	Check_overlay
+	FLDIGI_source
+	FLAMP_source
+	FLMSG_source
+	GPSD_install
+	chrony_setup
+	Xastir_install
+fi
+
+usage (){
+	echo "usage: install.sh (args)
+	Args can be:
+	FLDIGI : installs fldigi
+	FLAMP : installs flamp
+	FLMSG : installs flmsg
+	GPS : installs gps
+	CHRONY : installs/sets up chrony
+	xastir : installs xastir
+	-h shows this help and exits"
+	exit
+}
+
+
+############################
+## BUILD INSTALL FUNCTION ##
+############################
+# pauses between configure/make/install to allow user to double check install progress
+Build_Install (){
+	#note: static linking enabled, possibly do not need it as other libraries get loaded.
+	#./configure --enable-static
+	./configure
+	Press_Any_key
+	make
+	Press_Any_key
+	sudo make install
+	Press_Any_key
+}
+####################
+## Press continue ##
+####################
+Press_Any_key (){
+	if [$QUICK == 0] then
+		read -n 1 -s -r -p "Press any key to continue"
+		echo
+	fi
+}
+
 ####################
 ## Enable Sources ##
 ####################
-#BEFORE INSTALL, get all the deps for it!!! this takes editing the source list file and other fun stuff
-sudo cp /etc/apt/sources.list /etc/apt/sources.$FLDIGICUR.bkup
-#dirty way of doing it
+Enable_Sources () {
+	#maybe check to see if the source list has already been backed up or fixed and skip?
+	if grep "#deb-src" /etc/apt/sources.list
+	then
+		sudo cp /etc/apt/sources.list /etc/apt/sources.$FLDIGICUR.bkup
+		#dirty way of doing it
 echo  "deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi
 # Uncomment line below then 'apt-get update' to enable 'apt-get source'
 deb-src http://archive.raspbian.org/raspbian/ stretch main contrib non-free rpi
 " | sudo tee /etc/apt/sources.list
-echo "sources.list backed up to sources."$FLDIGICUR".bkup, please add any other sources from the old file to the new one that are not already in there"
-
+	
+	echo "sources.list backed up to sources."$FLDIGICUR".bkup, please add any other sources from the old file to the new one that are not already in there"
+	else
+		echo "sources already enabled"
+	fi
+	Press_Any_key
+}
 ############################
 ## Double check dtoverlay ##
 ############################
-#check the dtoverlay for draws, if not then prompt the user and open the file for editing
-if !(grep -x "dtoverlay=" /boot/config.txt && grep -x "dtoverlay=draws" /boot/config.txt); then
-	echo "draws not detected, opening boot config for editing, page down to the end and change 'udrc' to 'draws'"
-	echo
-	read -n 1 -s -r -p "Press any key to edit the file"
-	sudo nano /boot/config.txt
-	echo
-else
-	echo "dtoverlay lines found, continuing with install"
-fi
-read -n 1 -s -r -p "Press any key to continue"
-echo
-################
-## DEP & LIBS ##
-################
-#update and build the deps for fldigi
-sudo apt-get update
-sudo apt-get build-dep fldigi gpsd -y
-sudo apt-get remove imagemagick -y
+Check_overlay () {
+	#check the dtoverlay for draws, if not then prompt the user and open the file for editing
+	if !(grep -x "dtoverlay=" /boot/config.txt && grep -x "dtoverlay=draws" /boot/config.txt); then
+		echo "draws not detected, opening boot config for editing, page down to the end and change 'udrc' to 'draws'"
+		echo
+		read -n 1 -s -r -p "Press any key to edit the file"
+		sudo nano /boot/config.txt
+		echo
+	else
+		echo "dtoverlay lines found, continuing with install"
+	fi
+	#check for DRAWS:
+	echo "checking DRAWS eeprom version...."
+	draws="Digital Radio Amateur Work Station"
+	outvar=$(tr -d '\0' </sys/firmware/devicetree/base/hat/product)
+	if [ "$draws" == "$outvar" ]
+	then
+		echo "yep, you have a DRAWS Hat"
+	else
+		echo "nope, you do not have a DRAWS Hat! Modifications this script made may NOT work!!"
+	fi
+	Press_Any_key
+}
+
+###############
+## FLDIGI    ##
+###############
+FLDIGI_source () {
+	#grab any dep we forget:
+	sudo apt-get build-dep fldigi -y 
+	# from http://www.kk5jy.net/fldigi-build/, make sure they are installed
+	sudo apt-get install libfltk1.3-dev -y
+	sudo apt-get install libjpeg9-dev -y
+	sudo apt-get install libxft-dev -y
+	sudo apt-get install libxinerama-dev -y
+	sudo apt-get install libxcursor-dev -y
+	sudo apt-get install libsndfile1-dev -y
+	sudo apt-get install libsamplerate0-dev -y
+	sudo apt-get install portaudio19-dev -y
+	sudo apt-get install libusb-1.0-0-dev -y
+	sudo apt-get install libpulse-dev -y
+	echo "please check that all dep are installed"
+	Press_Any_key
+	cd ~
+	wget https://sourceforge.net/projects/fldigi/files/fldigi/fldigi-$FLDIGICUR.tar.gz
+	tar -zxvsf fldigi-$FLDIGICUR.tar.gz
+	cd fldigi-$FLDIGICUR
+	Build_Install
+	cp data/fldigi.desktop ~/Desktop/
+	cp data/flarq.desktop ~/Desktop/
+	if [ $TESTING == 0 ]
+	then
+		cd ~
+		cp ./DRAWS/fldigi/fldigi_def.xml ./.fldigi/fldigi_def.xml
+	fi
+}
+###############
+## FLAMP     ##
+###############
+FLAMP_source () {
+	cd ~
+	wget -N https://sourceforge.net/projects/fldigi/files/flamp/flamp-$FLAMPCUR.tar.gz
+	tar -zxvsf flamp-$FLAMPCUR.tar.gz
+	cd flamp-$FLAMPCUR
+	Build_Install
+	cp data/flamp.desktop ~/Desktop
+}
+###############
+## FLMSG     ##
+###############
+FLMSG_source () {
+	cd ~
+	wget https://sourceforge.net/projects/fldigi/files/flmsg/flmsg-$FLMSGCUR.tar.gz
+	tar -zxvsf flmsg-$FLMSGCUR.tar.gz
+	cd flmsg-$FLMSGCUR
+	Build_Install
+	cp data/flmsg.desktop ~/Desktop/
+}
 ##########
 ## GPSD ##
 ##########
-# gpsd from repository is outdated, download new one and compile/install
-cd ~
-wget http://download.savannah.nongnu.org/releases/gpsd/gpsd-3.18.1.tar.gz
-tar -zxvsf gpsd-3.18.1
-cd gpsd-3.18.1
-scons && scons check && sudo scons udev-install
+GPSD_install (){
 
-# favorate apps:
-sudo apt-get install geany -y
-# from nwdigital radio install file:
-# install needed stuff:
-sudo apt-get install rsync build-essential autoconf dh-autoreconf automake libtool git libasound2-dev libncurses5-dev -y
+	sudo apt-get remove gpsd -y
+	# gpsd from repository is outdated, download new one and compile/install
+	cd ~
+	wget http://download.savannah.nongnu.org/releases/gpsd/gpsd-3.18.1.tar.gz
+	tar -zxvsf gpsd-3.18.1.tar.gz
+	cd gpsd-3.18.1
+	scons && scons check && sudo scons udev-install
+	cd ~
+	sudo cp ./DRAWS/gpsd /etc/default/gpsd
+	sudo systemctl unmask gpsd.service && sudo systemctl unmask gpsd.socket && sudo systemctl enable gpsd && sudo systemctl restart gpsd
+	echo "GPS daemon ready, run 'gpsmon' to check gps, if it doesn't work reboot"
+	Press_Any_key
+}
+###################
+## CHRONY DAEMON ##
+###################
+chrony_setup (){
+	sudo apt-get install chrony -y
+	sudo cp ./DRAWS/chrony.conf /etc/chrony/chrony.conf
+	sudo systemctl enable chrony && sudo systemctl restart chrony && systemctl status chrony
+	echo "Chrony is ready, run 'chronyc sources' to verify"
+	Press_Any_key
+}
+###############
+## XASTIR    ##
+###############
+Xastir_install () {
+	#note: use graphicsmagick, breaks with imagemagic
+	sudo apt-get remove imagemagick -y
+	#build list taken from https://xastir.org/index.php/HowTo:Raspbian_Jessie
+	sudo apt-get install xorg-dev graphicsmagick gv libmotif-dev libcurl4-openssl-dev -y
+	sudo apt-get install libpcre3-dev libproj-dev libdb5.3-dev python-dev libax25-dev libwebp-dev -y
+	sudo apt-get install shapelib libshp-dev festival festival-dev libgeotiff-dev libgraphicsmagick1-dev -y
+	sudo apt-get install xfonts-100dpi xfonts-75dpi
+	xset +fp /usr/share/fonts/X11/100dpi,/usr/share/fonts/X11/75dpi
+	cd ~
+	git clone https://github.com/Xastir/Xastir.git
+	cd xastir
+	./bootstrap.sh
+	./configure CPPFLAGS="-I/usr/include/geotiff"
+	#ask if it configured correctly
+	read -n 1 -s -r -p "Check configuration, Press any key to continue or ctrl+c to exit"
+	echo
+	make
+	sudo make install
+	if [ $TESTING == 0 ]
+	then
+		#copy all config files
+		cd ~
+		cp ./DRAWS/direwolf.conf ./direwolf.conf
+		cp ./DRAWS/xastir/* ./.xastir/config
+
+	fi
+	
+}
+
+
+
+
 # libraries:
 #apparently some files are missing, adding in a bunch of dependencies that might be needed from http://www.kk5jy.net/fldigi-build/:
-sudo apt-get install libfltk1.3-dev libjpeg9-dev libxft-dev libxinerama-dev libxcursor-dev libsndfile1-dev libsamplerate0-dev portaudio19-dev libusb-1.0-0-dev libpulse-dev libmotif-dev chrony graphicsmagick libgraphicsmagick1-dev festival festival-dev shapelib libshp-dev libpcre3-dev libproj-dev libdb-dev python-dev libwebp-dev libgeotiff-dev -y
+#note: with beta 6 image this breaks, have no idea why, might need to break this up into smaller chunks and verify the install better.
+#sudo apt-get install libfltk1.3-dev libjpeg9-dev libxft-dev libxinerama-dev libxcursor-dev libsndfile1-dev libsamplerate0-dev portaudio19-dev libusb-1.0-0-dev libpulse-dev libmotif-dev chrony graphicsmagick libgraphicsmagick1-dev festival festival-dev shapelib libshp-dev libpcre3-dev libproj-dev libdb-dev python-dev libwebp-dev libgeotiff-dev libtiff-dev -y
 #gps specific stuff:
-sudo apt-get install gpsman gpsd-clients python-gps pps-tools libgps-dev
+#sudo apt-get install gpsman gpsd-clients python-gps pps-tools libgps-dev
+
 #make sure in home directory
-cd ~
+#cd ~
+
 #grab the scripts
-git clone https://github.com/nwdigitalradio/n7nix
-#install the base files
-#cd n7nix/config
-#sudo ./core_install.sh
+#git clone https://github.com/nwdigitalradio/n7nix
 #################################
 ###########  NOTE  ##############
 #################################
 # nwdigital radio scripts will not be used in this, I will be phasing them out as they might have unintended affects
 # the scripts will still be downloaded for testing purposes and if other features not covered in this script
 # (ie ax.25 stacks, rms, etc) are needed. Audio level setting will come from supplied file with this repository
-
-###############
-## FLDIGI    ##
-###############
-cd ~
-wget https://sourceforge.net/projects/fldigi/files/fldigi/fldigi-$FLDIGICUR.tar.gz
-tar -zxvsf fldigi-$FLDIGICUR.tar.gz
-cd fldigi-$FLDIGICUR
-Build_Install
-cp data/fldigi.desktop ~/Desktop/
-cp data/flarq.desktop ~/Desktop/
-###############
-## FLAMP     ##
-###############
-cd ~
-wget -N https://sourceforge.net/projects/fldigi/files/flamp/flamp-$FLAMPCUR.tar.gz
-tar -zxvsf flamp-$FLAMPCUR.tar.gz
-cd flamp-$FLAMPCUR
-Build_Install
-cp data/flamp.desktop ~/Desktop
-###############
-## FLMSG     ##
-###############
-cd ~
-wget https://sourceforge.net/projects/fldigi/files/flmsg/flmsg-$FLMSGCUR.tar.gz
-tar -zxvsf flmsg-$FLMSGCUR.tar.gz
-cd flmsg-$FLMSGCUR
-Build_Install
-cp data/flmsg.desktop ~/Desktop/
-
-###############
-## XASTIR    ##
-###############
-#note: use graphicsmagick, install above with all dep, remove imagemagick
-cd ~
-git clone https://github.com/xastir/xastir
-cd xastir
-./bootstrap.sh
-./configure
-#ask if it configured correctly
-read -n 1 -s -r -p "Check configuration, Press any key to continue or ctrl+c to exit"
-echo
-make
-sudo make install
-
-##################
-## config files ##
-##################
-if [ $TESTING == 0 ]
-then
-	#copy all config files
-	cd ~
-	cp ./DRAWS/direwolf.conf ./direwolf.conf
-	cp ./DRAWS/fldigi/fldigi_def.xml ./.fldigi/fldigi_def.xml
-	cp ./DRAWS/xastir/* ./.xastir/config
-
-fi
-sudo cp ./DRAWS/gpsd /etc/default/gpsd
-sudo cp ./DRAWS/chrony.conf /etc/chrony/chrony.conf
-#########################
-## GPS & CHRONY DAEMON ##
-#########################
-
-sudo systemctl enable gpsd && sudo systemctl restart gpsd
-sudo systemctl enable chrony && sudo systemctl restart chrony && systemctl status chrony
-
-echo "GPS daemon ready, run 'gpsmon' to check gps"
-echo "Chrony is ready, run 'chronyc sources' to verify"
-read -n 1 -s -r -p "Press any key to continue"
-echo
-#check for DRAWS:
-echo "checking DRAWS eeprom version...."
-draws="Digital Radio Amateur Work Station"
-outvar=$(tr -d '\0' </sys/firmware/devicetree/base/hat/product)
-if [ "$draws" == "$outvar" ]
-then
-	echo "yep, you have a DRAWS Hat"
-else
-	echo "nope, you do not have a DRAWS Hat! Modifications this script made may NOT work!!"
-fi
